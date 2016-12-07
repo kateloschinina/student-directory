d681baacce8e9419c19ed46156098dd07ce71051
kateloschinina <ekaterina.loshchinina@gmail.com>
2016-12-07 15:43:40 +0000

    Exercises. Number 6

--- directory.rb
+++ directory.rb
@@ -17,28 +17,28 @@ def input_students
     students
 end
 
-def print_header
-    puts "The students of Villians Academy"
-    puts "-----------------"
+def print_header(output_width = 86)
+    puts "The students of Villians Academy".center(output_width)
+    puts "-----------------".center(output_width)
 end
 
-def print(students, specific_letter = nil, specific_length = 12)
+def print(students, specific_letter = nil, specific_length = 12, output_width = 86)
     i = 0
     while !students[i].nil? do
         #puts "st = #{students[i]}"
         if ((specific_letter == nil) or (students[i][:name][0].downcase == specific_letter.downcase)) and (students[i][:name].length < specific_length)
-          puts  "#{i+1}: Student name: #{students[i][:name]},",
-                " Student cohort: #{students[i][:cohort]},",
-                " Student hobby: #{students[i][:hobby]},",
-                " Student country of birth: #{students[i][:country_of_birth]},",
-                " Student height: #{students[i][:height]}."
+          puts  "#{i+1}:"+"Student's name: #{students[i][:name]},".center(output_width-2),
+                " Student's cohort: #{students[i][:cohort]},".center(output_width,'.'),
+                " Student's hobby: #{students[i][:hobby]},".center(output_width,'.'),
+                " Student's country of birth: #{students[i][:country_of_birth]},".center(output_width,'.'),
+                " Student's height: #{students[i][:height]}.".center(output_width,'.')
         end
         i += 1
     end
 end
 
-def print_footer(students)
-  puts "Overall, we have #{students.length} great students."
+def print_footer(students, output_width = 86)
+  puts "Overall, we have #{students.length} great students.".center(output_width)
 end
 
 students = input_students
