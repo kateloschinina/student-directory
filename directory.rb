d14c700f06b85f9e9cfce61d5027af55c39ee4d6
kateloschinina <ekaterina.loshchinina@gmail.com>
2016-12-07 23:01:35 +0000

    Interative menu

--- directory.rb
+++ directory.rb
@@ -1,10 +1,10 @@
+@students = [] # an empty array accessible to all methods
+
 def input_students
     puts "Please enter the names of the students and their corresponding cohorts",
     "(Note! (1) cohort should be one of the following (with exact spelling): January, February, March, April, May, June, July, August, September, October, November, December",
     "(2) cohort will be authomatically assigned to November if you enter an empty string)"
     puts "To finish, just hit return twice"
-    # create an empty array
-    students = []
     # get the first name
     puts "Student's name:"
     name = gets.slice(0..-2)
@@ -28,14 +28,12 @@ def input_students
         end
       end
       # add the student hash to the array
-      students << {name: name, cohort: cohort.to_sym, hobby: :coding, country_of_birth: :UK, height: :"172"}
-      puts "Now we have #{students.count} student#{students.length > 1 ? "s" : ""}"
+      @students << {name: name, cohort: cohort.to_sym, hobby: :coding, country_of_birth: :UK, height: :"172"}
+      puts "Now we have #{@students.count} student#{@students.length > 1 ? "s" : ""}"
       # get another name and cohort from the user
       puts "Student's name:"
       name = gets.slice(0..-2)
     end
-    # return the array of students
-    students
 end
 
 def print_header(output_width = 86)
@@ -43,33 +41,70 @@ def print_header(output_width = 86)
     puts "-----------------".center(output_width)
 end
 
-def print(students, output_width = 86, specific_letter = nil, specific_length = 12)
+def print_students_list(output_width = 86, specific_letter = nil, specific_length = 12)
     i = 0
     # handy function that sorts hash by cohort - to group them later
-    students.sort_by! { |k| k[:cohort] }
+    @students.sort_by! { |k| k[:cohort] }
 
-    while !students[i].nil? do
+    while !@students[i].nil? do
         # check all requested requrements (starts from letter, length) by default: letter is nil, length is 12
-          if ((specific_letter == nil) or (students[i][:name][0].downcase == specific_letter.downcase)) and (students[i][:name].length < specific_length)
+          if ((specific_letter == nil) or (@students[i][:name][0].downcase == specific_letter.downcase)) and (@students[i][:name].length < specific_length)
           # puts cohort name if starts next cohort in sorted hash
-          if (i==0) or (students[i][:cohort] != students[i-1][:cohort])
-            puts " Cohort: #{students[i][:cohort]}".center(output_width,'.')
+          if (i==0) or (@students[i][:cohort] != @students[i-1][:cohort])
+            puts " Cohort: #{@students[i][:cohort]}".center(output_width,'.')
           end
           # remaining output
-          puts  "#{i+1}:"+"Student's name: #{students[i][:name]},".center(output_width-2),
-                " student's hobby: #{students[i][:hobby]},".center(output_width),
-                " student's country of birth: #{students[i][:country_of_birth]},".center(output_width),
-                " student's height: #{students[i][:height]}.".center(output_width),
+          puts  "#{i+1}:"+"Student's name: #{@students[i][:name]},".center(output_width-2),
+                " student's hobby: #{@students[i][:hobby]},".center(output_width),
+                " student's country of birth: #{@students[i][:country_of_birth]},".center(output_width),
+                " student's height: #{@students[i][:height]}.".center(output_width),
                 ""
             end
         i += 1
     end
 end
 
-def print_footer(students, output_width = 86)
-  puts "Overall, we have #{students.length} great student#{students.length > 1 ? "s" : ""}.".center(output_width)
+def print_footer(output_width = 86)
+  puts "Overall, we have #{@students.length} great student#{@students.length > 1 ? "s" : ""}.".center(output_width)
 end
 
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    #input the students
+    input_students
+  when "2"
+    #show the students
+    show_students
+  when "9"
+    exit # in this case the program will terminate
+  else
+    puts "I don't know what you mean, try again"
+  end
+end
+
+def interative_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+interative_menu
+
+=begin
 students = input_students
 string_size = 86
 if students.empty?
@@ -80,3 +115,4 @@ else
   print(students, string_size)
   print_footer(students, string_size)
 end
+=end
